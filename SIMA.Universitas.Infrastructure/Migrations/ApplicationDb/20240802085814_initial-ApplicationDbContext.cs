b046d6737db0d67b86cf98a04038d08bbc5ccc0d
azampunya25 <zamzam.ahmad@finansia.com>
2024-08-02 19:44:06 +0700

    appsetting

---
+++ SIMA.Universitas.Infrastructure/Migrations/ApplicationDb/20240802085814_initial-ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SIMA.Universitas.Infrastructure.Migrations.ApplicationDb
+{
+    /// <inheritdoc />
+    public partial class initialApplicationDbContext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
